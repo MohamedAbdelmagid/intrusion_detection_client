7d0e41dadb76a2ce3917c013a2c4e575a77971ca
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2021-04-28 16:04:22 -0500

    final code

--- client.py
+++ client.py
@@ -10,8 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clientDB.sqlite3'
 
 db = SQLAlchemy(app)
 
-server_ip_address = '192.168.0.100'
-# server_ip_address = '192.168.1.36'
+server_ip_address = '192.168.1.35'
+# server_ip_address = '192.168.0.100'
 server_port = '5000'
 
 class Device(db.Model):
@@ -32,10 +32,15 @@ class Device(db.Model):
             'type': self.type,
             'ip_address': self.ip_address,
             'port': self.port,
-            'event': self.status,
+            'status': self.status,
         }
         return device
 
+# Delete old database
+db.drop_all()
+
+# Create new database
+db.create_all()
 
 @app.route('/test/<event>', methods=['GET'])
 def authenticate_ip(event):
@@ -44,14 +49,24 @@ def authenticate_ip(event):
     # Find device with this ip in the database
     device = Device.query.filter_by(ip_address=current_device_IP).first()
     if device:
-        if device.status == "blocked":
+        if device.status != 'trusted' and event == 'abnormal':
+            device.status = 'blocked'
+            db.session.commit()
+            # Make a request to the server to prodcast the ip address
+            res = requests.get('http://' + server_ip_address + ':' + server_port + '/' + event + '/' + current_device_IP)
+            if res.status_code == 200:
+                abort(401)
+            else:
+                return jsonify({ 'message': "Something went wrong, try again later!!" }), 403
+
+        elif device.status == "blocked":
             abort(401)
         else:
             return jsonify({ 'message': "Hi, you're welcome :)" })
 
     else:
         # Make a request to the server to check the ip address
-        res = requests.get('http://' + server_ip_address + ':' + server_port + '/' + current_device_IP + '/' + event)
+        res = requests.get('http://' + server_ip_address + ':' + server_port + '/' + event + '/' + current_device_IP)
 
         if res.status_code == 200:
             response = res.json()
@@ -67,23 +82,26 @@ def authenticate_ip(event):
             else:
                 return jsonify({ 'message': "Hi, you're welcome :)" })
         else:
-            print('!!!! Request to the server failed !!!!!')
+            print('#### Device with IP ' + current_device_IP + ' is NOT added!!!! Problem with the server!!!')
 
 @app.route('/add/<address>/<status>', methods=['GET', 'POST'])
 def add_new_device(address, status='unknown'):
     # Find device with this ip in the database
     device = Device.query.filter_by(ip_address=address).first()
     if device:
-        return jsonify({ 'msg': 'Already exist in the database', 'device': device.to_dict() }), 303
+        device.status = status
+        db.session.commit()
 
-    # Add new device to database
-    newDevice = Device(ip_address=address, status=status)
-    db.session.add(newDevice)
-    db.session.commit()
+        return jsonify({ 'msg': 'Already exist in the database', 'device': device.to_dict() }), 202
+    else:
+        # Add new device to database
+        device = Device(ip_address=address, status=status)
+        db.session.add(device)
+        db.session.commit()
 
-    return jsonify({ 'device': newDevice.to_dict()}), 201
+        return jsonify({ 'device': device.to_dict()}), 201
 
 
 if __name__ == '__main__':
     # app.run(debug=True)  # important to mention debug=True
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=3000)
